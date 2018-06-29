7fa61f8eaf73beccad5f28e82869e839d173a837
Bobby Martin <robert.martin.619@gmail.com>
2018-06-29 09:41:40 -0700

    Add new keg list components to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import KegList from './KegList/KegList';
+
 
 function App(){
   return (
     <div>
       <Switch>
-        
+        <Route exact path='/' component={KegList} />
       </Switch>
     </div>
   );
