200a068857597800a068bdfbc0c72118e6671663
Bobby Martin <robert.martin.619@gmail.com>
2018-06-29 14:00:45 -0700

    Add filler data

--- src/components/KegList/KegList.jsx
+++ src/components/KegList/KegList.jsx
@@ -5,10 +5,72 @@ import AddKegButton from './AddKegButton/AddKegButton';
 
 import styles from './KegList.css';
 
+const state = {
+  masterKegList: [
+    {
+      name: 'Ruby Zozzle',
+      brewer: 'Hi-Wheel',
+      description: 'Sparkling Wine & Grapefruit',
+      abv: '6.8%',
+      price: '7',
+      remaining: '20'
+    },
+    {
+      name: 'Tart N Juicy',
+      brewer: 'Epic',
+      description: 'Sour IPA',
+      abv: '4.5%',
+      price: '6',
+      remaining: '60'
+    },
+    {
+      name: 'Hamm\'s',
+      brewer: 'Miller/Coors',
+      description: 'American Lager',
+      abv: '4.7%',
+      price: '3',
+      remaining: '65'
+    },
+    {
+      name: 'Prismatic',
+      brewer: 'Ninkasi',
+      description: 'Juicy IPA',
+      abv:  '5.9%',
+      price: '6',
+      remaining: '75'
+    },
+    {
+      name: 'Juicy Haze',
+      brewer: 'New Belgium',
+      description: 'India Pale Ale',
+      abv:  '7.5%',
+      price: '6',
+      remaining: '18'
+    },
+    {
+      name: '8 Hop',
+      brewer: 'New Belgium',
+      description: 'Pale Ale',
+      abv:  '5.5%',
+      price: '6',
+      remaining: '58'
+    }
+  ]
+};
+
 function KegList() {
   return (
     <div className={styles.kegList}>
-      <KegListItem />
+      {state.masterKegList.map((beer, index) =>
+        <KegListItem
+          name={beer.name}
+          brewer={beer.brewer}
+          description={beer.description}
+          abv={beer.abv}
+          price={beer.price}
+          remaining={beer.remaining}  
+          key={index}/>
+      )}
       <AddKegButton />
     </div>
   );
--- src/components/KegList/KegListItem/KegListItem.jsx
+++ src/components/KegList/KegListItem/KegListItem.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './KegListItem.css';
 import SellKegButton from './../SellKegButton/SellKegButton';
 
-function KegListItem() {
+function KegListItem(props) {
   return (
     <div className={styles.kegListItem}>
-      <div className={styles.kegName}>Keg Name goes Here <span><SellKegButton/></span></div>
+      <div className={styles.kegName}>{props.name}<span><SellKegButton/></span></div>
+      <ul>
+        <li>{props.brewer}</li>
+        <li>{props.description}</li>
+        <li>{props.abv}</li>
+        <li>{props.price}</li>
+        <li>{props.remaining}</li>
+      </ul>
     </div>
   );
 }
+
+KegListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  brewer: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  abv: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  remaining: PropTypes.string.isRequired
+};
 
 export default KegListItem;
