31928332cfbbc1be01d6326f2c3956b82335988d
Bobby Martin <robert.martin.619@gmail.com>
2018-06-29 14:33:04 -0700

    Add styles to keg list component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,28 @@ import Header from './Header/Header';
 import KegList from './KegList/KegList';
 import NewKeg from './NewKeg/NewKeg';
 
+import jerry from './../images/jerry.png';
+
 
 function App(){
   const appStyles = {
-    fontFamily: 'sans-serif',
+    fontFamily: 'Helvetica, sans-serif',
     backgroundColor: '#513E13',
     color: 'white',
-    height: '100%'
+    height: '100%',
+    position: 'relative',
+  };
+  const jerryStyles = {
+    position: 'absolute',
+    margin: 'auto',
+    zIndex: '0',
+    left: '0',
+    right: '0'
   };
   return (
     <div style={appStyles}>
       <Header />
+      <img style={jerryStyles} src={jerry} />
       <Switch>
         <Route exact path='/' component={KegList} />
         <Route path='/newkeg' component={NewKeg} />
