00e43363eb0d7731074b9b2b683d970336590055
Bobby Martin <robert.martin.619@gmail.com>
2018-06-29 11:49:20 -0700

    Add sell keg button component

--- src/components/KegList/KegList.jsx
+++ src/components/KegList/KegList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import KegListItem from './KegListItem/KegListItem';
 import AddKegButton from './AddKegButton/AddKegButton';
+import SellKegButton from './SellKegButton/SellKegButton';
 function KegList() {
   return (
     <div>
       <KegListItem />
+      <SellKegButton />
       <AddKegButton />
     </div>
   );
---
+++ src/components/KegList/SellKegButton/SellKegButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SellKegButton() {
+  return(
+    <div>
+      Sell Keg
+    </div>
+  );
+}
+
+export default SellKegButton;
