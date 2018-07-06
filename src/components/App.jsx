e32558b10c85fe6ecc54bff10dc8bd5c772a3854
Bobby Martin <robert.martin.619@gmail.com>
2018-07-06 08:37:24 -0700

    Make App component stateful

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,31 +8,39 @@ import NewKeg from './NewKeg/NewKeg';
 import jerry from './../images/jerry.png';
 
 
-function App(){
-  const appStyles = {
-    fontFamily: 'Helvetica, sans-serif',
-    backgroundColor: '#513E13',
-    color: 'white',
-    height: '100%',
-    position: 'relative',
-  };
-  const jerryStyles = {
-    position: 'absolute',
-    margin: 'auto',
-    zIndex: '0',
-    left: '0',
-    right: '0'
-  };
-  return (
-    <div style={appStyles}>
-      <Header />
-      <img style={jerryStyles} src={jerry} />
-      <Switch>
-        <Route exact path='/' component={KegList} />
-        <Route path='/newkeg' component={NewKeg} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+  render() {
+    const appStyles = {
+      fontFamily: 'Helvetica, sans-serif',
+      backgroundColor: '#513E13',
+      color: 'white',
+      height: '100%',
+      position: 'relative',
+    };
+    const jerryStyles = {
+      position: 'absolute',
+      margin: 'auto',
+      zIndex: '0',
+      left: '0',
+      right: '0'
+    };
+    return (
+      <div style={appStyles}>
+        <Header />
+        <img style={jerryStyles} src={jerry} />
+        <Switch>
+          <Route exact path='/' component={KegList} />
+          <Route path='/newkeg' component={NewKeg} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
