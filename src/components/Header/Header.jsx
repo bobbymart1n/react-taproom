6f60073e39175ca23146325f24ad6f7c4423bba0
Bobby Martin <robert.martin.619@gmail.com>
2018-07-06 10:34:24 -0700

    Add link to header text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.css';
 
 function Header() {
+  const linkStyle = {
+    textDecoration: 'none'
+  };
   return(
     <div className={styles.nav}>
-      <h1>Jerry's Tap Room</h1>
+      <Link to='/' style={linkStyle}><h1>Jerry's Tap Room</h1></Link>
     </div>
   );
 }
